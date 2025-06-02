35362ac7f8e44b907e85a40489f573c99243552e
Nicholas Fiorentini <lordgordon@users.noreply.github.com>
2025-06-02 16:51:37 +0200

    refactoring unit tests

--- src/vector.rs
+++ src/vector.rs
@@ -27,13 +27,22 @@ fn index_of_best_vector(m: ArrayView2<f64>) -> usize {
     norms.argmin().unwrap()
 }
 
+fn rescale_value(x: f64, min_val: f64, max_val: f64) -> f64 {
+    // TODO: address division by zero, guarantee max > min
+    (x - min_val) / (max_val - min_val)
+}
+
 // fn rescale_vector(v: ArrayView1<f64>) -> ArrayView1 {
 //     let min_val = v.max().unwrap();
 //     let max_val = v.min().unwrap();
+
+//     v.for_each(f);
 // }
 
 #[cfg(test)]
 mod tests {
+    // TODO: https://crates.io/crates/proptest
+
     use ndarray::array;
 
     use super::*;
@@ -41,31 +50,57 @@ mod tests {
     const SQRT2: f64 = std::f64::consts::SQRT_2;
 
     #[test]
-    fn experiment_with_ndarray_single_norm() {
-        let x1 = array![0.];
-        assert_eq!(l2_norm(x1.view()), 0.);
-
-        let x2 = array![2.];
-        assert_eq!(l2_norm(x2.view()), 2.);
-
-        let x3 = array![1., 1.];
-        assert_eq!(l2_norm(x3.view()), SQRT2);
+    fn test_l2_norm_single_value_zero() {
+        let v = array![0.];
+        assert_eq!(l2_norm(v.view()), 0.);
     }
 
     #[test]
-    fn experiment_with_ndarray_matrix() {
-        let sqrt3: f64 = (3.0_f64).sqrt();
-
-        let m1 = array![[1., 1.], [0., 0.], [1., 0.], [0., 1.],];
-        assert_eq!(l2_norm_vectors(m1.view()), array![SQRT2, 0., 1., 1.]);
-        assert_eq!(index_of_best_vector(m1.view()), 1);
-
-        let m1 = array![[1., 1.], [2., 0.], [1., 0.], [0., 2.],];
-        assert_eq!(l2_norm_vectors(m1.view()), array![SQRT2, 2., 1., 2.]);
-        assert_eq!(index_of_best_vector(m1.view()), 2);
-
-        let m1 = array![[1., 1., 1.], [2., 0., 0.], [1., 0., 1.], [1., 0., 1.],];
-        assert_eq!(l2_norm_vectors(m1.view()), array![sqrt3, 2., SQRT2, SQRT2,]);
-        assert_eq!(index_of_best_vector(m1.view()), 2);
+    fn test_l2_norm_single_value_exact() {
+        let v = array![2.];
+        assert_eq!(l2_norm(v.view()), 2.);
     }
+
+    #[test]
+    fn test_l2_norm_vector2_ones() {
+        let v = array![1., 1.];
+        assert_eq!(l2_norm(v.view()), SQRT2);
+    }
+
+    #[test]
+    fn test_l2_norm_vector_zeros() {
+        let v = Array1::<f64>::zeros(5);
+        assert_eq!(l2_norm(v.view()), 0.);
+    }
+
+    #[test]
+    fn test_l2_norm_vector_ones() {
+        let v = Array1::<f64>::ones(5);
+        assert_eq!(l2_norm(v.view()), (5.0_f64).sqrt());
+    }
+
+    #[test]
+    fn test_best_vector_from_matrix_success_1() {
+        let m = array![[1., 1.], [0., 0.], [1., 0.], [0., 1.],];
+        assert_eq!(l2_norm_vectors(m.view()), array![SQRT2, 0., 1., 1.]);
+        assert_eq!(index_of_best_vector(m.view()), 1);
+    }
+
+    #[test]
+    fn test_best_vector_from_matrix_success_2() {
+        let m = array![[1., 1.], [2., 0.], [1., 0.], [0., 2.],];
+        assert_eq!(l2_norm_vectors(m.view()), array![SQRT2, 2., 1., 2.]);
+        assert_eq!(index_of_best_vector(m.view()), 2);
+    }
+
+    #[test]
+    fn test_best_vector_from_matrix_success_3() {
+        let sqrt3: f64 = (3.0_f64).sqrt();
+        let m = array![[1., 1., 1.], [2., 0., 0.], [1., 0., 1.], [1., 0., 1.],];
+        assert_eq!(l2_norm_vectors(m.view()), array![sqrt3, 2., SQRT2, SQRT2,]);
+        assert_eq!(index_of_best_vector(m.view()), 2);
+    }
+
+    #[test]
+    fn rescale_value_success() {}
 }
