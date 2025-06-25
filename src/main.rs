30629432064d6b2b3ec79e2fafc3ac71de63da37
Nicholas Fiorentini <lordgordon@users.noreply.github.com>
2025-06-25 23:31:37 +0200

    feat: minimal cli with clap. Shows version and help

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello rustimizer");
 }
